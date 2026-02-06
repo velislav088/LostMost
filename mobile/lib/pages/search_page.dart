0e59a2089ac1cb0673aa44b76f8b45aadcf1527c
Velislav Donchev <156554141+velislav088@users.noreply.github.com>
2026-02-06 22:58:54 +0200

    Refactored search_page.dart to include animations for search icon and text

--- mobile/lib/pages/search_page.dart
+++ mobile/lib/pages/search_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mobile/theme/app_localizations.dart';
+import 'package:mobile/widgets/animations_util.dart';
 
 class SearchPage extends StatelessWidget {
   const SearchPage({super.key});
@@ -7,16 +8,29 @@ class SearchPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Scaffold(
     appBar: AppBar(title: Text(AppLocalizations.of(context, 'search_title'))),
-    body: Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Icon(Icons.search, size: 100),
-          Text(
-            AppLocalizations.of(context, 'search_page'),
-            style: const TextStyle(fontSize: 24),
+    body: SafeArea(
+      child: FadeInAnimation(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ScaleInAnimation(
+                child: Icon(
+                  Icons.search,
+                  size: 100,
+                  color: Theme.of(
+                    context,
+                  ).colorScheme.primary.withValues(alpha: 0.5),
+                ),
+              ),
+              const SizedBox(height: 24),
+              Text(
+                AppLocalizations.of(context, 'search_page'),
+                style: Theme.of(context).textTheme.headlineSmall,
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     ),
   );
