900e2740e4007b3845c72c4adbe01c6bb7a5e87b
Velislav Donchev <156554141+velislav088@users.noreply.github.com>
2026-02-09 19:39:12 +0200

    Added constants for app configuration in app_constants.dart

---
+++ mobile/lib/config/app_constants.dart
@@ -0,0 +1,9 @@
+class AppConstants {
+  static const int mqttPort = 8883;
+  static const String defaultDeviceId = 'esp32_001';
+  static const int mqttKeepAlive = 20;
+  static const Duration scanInterval = Duration(seconds: 10);
+
+  static const int rssiCloseThreshold = -60;
+  static const int rssiNearbyThreshold = -80;
+}
