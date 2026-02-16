3945286ec1dfea5bd0d7cfd95a386946db4be00f
Velislav Donchev <156554141+velislav088@users.noreply.github.com>
2026-02-16 22:15:32 +0200

    Remove mock rssi close threshold

--- mobile/lib/config/app_constants.dart
+++ mobile/lib/config/app_constants.dart
@@ -4,6 +4,5 @@ class AppConstants {
   static const int mqttKeepAlive = 20;
   static const Duration scanInterval = Duration(seconds: 10);
 
-  static const int rssiCloseThreshold = -60;
   static const int rssiNearbyThreshold = -80;
 }
